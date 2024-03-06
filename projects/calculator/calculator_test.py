2de13a3beb2f8798b70d7dd96928c5bdfe6cd7e5
tony.moon <tony.moon.kr@gmail.com>
2024-03-06 21:44:13 +0100

    init

---
+++ projects/calculator/calculator_test.py
@@ -0,0 +1,12 @@
+import unittest
+from projects.calculator.calculator import Calculator
+
+class TestSum(unittest.TestCase):
+    def test_sum(self):
+        print("test")
+        calculator = Calculator()
+        self.assertEqual(calculator.add(1,2),3)
+
+if __name__ == '__main__':
+   unittest.main()
+    
